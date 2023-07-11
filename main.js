8d90f13c73042e58dd1bd3647da69a8728a00711
Catalina Gatica <cata.gatica.f@hotmail.com.ar>
2023-07-11 16:33:14 -0300

    segunda entrega

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+//PRE ENTREGA 1
+/*
+function entrega1(){
 let saludo = alert(
   "Gracias por comprar con nosotros! A continuación deberá brindarnos información sobre su compra"
 );
@@ -37,4 +40,61 @@ if (formaDePagoElegida >4){
     break;
 }
 }
+}
+entrega1();
+*/
 
+//PRE ENTREGA 2
+
+
+
+function entrega2() {
+  let saludo = alert(
+    "Hola! gracias por utilizar nuestro software para actualizar su inventario"
+  );
+  const productos = [];
+  class Producto {
+    constructor(nombre, precio, stock, fechaDeIngreso) {
+      (this.nombre = nombre), (this.precio = precio * 1.21);
+      this.stock = stock;
+      this.fechaDeIngreso = new Date(fechaDeIngreso);
+    }
+  }
+  for (let i = 0; i < 4; i++) {
+    let nombre = prompt("ingrese el nombre del producto");
+    let precio = parseInt(prompt("Ingrese el precio por unidad"));
+    let stock = parseInt(prompt("ingrese la cantidad da unidades ingresadas"));
+    let fechaDeIngreso = prompt(
+      "ingrese la fecha de carga del producto en el formato YYYY-MM-DD"
+    );
+    let nuevoProducto = new Producto(nombre, precio, stock, fechaDeIngreso);
+    productos.push(nuevoProducto);
+  }
+  console.log(productos);
+
+  let totalPrecios = 0;
+  for (const producto of productos) {
+    totalPrecios += producto.precio * producto.stock;
+  }
+  console.log("el total invertido es de $" + totalPrecios);
+  let productoABuscar = prompt(
+    "ingrese el nombre del prod que desea buscar en el inventario"
+  );
+  const busqueda = productos.find(
+    (producto) => producto.nombre === productoABuscar
+  );
+  console.log(busqueda);
+  const ordenarProductos = parseInt(
+    prompt(
+      "Elija si desea ordenar los productos: \n 1)de menor a mayor precio. \n 2) de mayor a menor precio"
+    )
+  );
+  if (ordenarProductos === 1) {
+    return productos.sort((a, b) => a.precio - b.precio);
+  } else if (ordenarProductos === 2) {
+    return productos.sort((a, b) => b.precio - a.precio);
+  }
+  console.log(productos);
+
+}
+entrega2();
